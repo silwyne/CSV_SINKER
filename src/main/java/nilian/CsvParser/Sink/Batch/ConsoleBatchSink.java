26540e89c75ff393b4d74b011222ace047ee5e9b
smh_tabatabaei <tabatabei.smh@yaftar.ir>
2024-11-17 19:53:56 +0330

    [REMOVED] unused Classes

--- src/main/java/nilian/CsvParser/Sink/Batch/ConsoleBatchSink.java
+++
@@ -1,7 +0,0 @@
-package nilian.CsvParser.Sink.Batch;
-public class ConsoleBatchSink<T> implements SinkBatchFunction<T> {
-    @Override
-    public void sinkData(T data) {
-        System.out.println(data);
-    }
-}
--- src/main/java/nilian/CsvParser/Sink/Batch/SinkBatchFunction.java
+++
@@ -1,5 +0,0 @@
-package nilian.CsvParser.Sink.Batch;
-
-public interface SinkBatchFunction<T> {
-    public void sinkData(T data) ;
-}
